b002a067e1567b7447fb0c7c6925b456e28977ec
Steven Kerr <sker078@aucklanduni.ac.nz>
2020-08-24 20:29:04 +1200

    Added movement to the ship

---
+++ Argon_Assalt_4/Assets/Player.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float Speed = 20f;
+    [Tooltip("In m")] [SerializeField] float xRange = 15f;
+    [Tooltip("In m")] [SerializeField] float yRange = 15f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float yThrow = CrossPlatformInputManager.GetAxis("Vertical");
+
+        float xOffset = xThrow * Speed * Time.deltaTime;
+        float yOffset = yThrow * Speed * Time.deltaTime;
+
+        float rawXPos = transform.localPosition.x + xOffset;
+        float rawYPos = transform.localPosition.y + yOffset;
+
+        float clampedXPos = Mathf.Clamp(rawXPos, -xRange, xRange);
+        float clampedYPos = Mathf.Clamp(rawYPos, -yRange, yRange);
+
+        print(xThrow * 50);
+
+        float clampedZRoll = Mathf.Clamp(xThrow * 50, -50, 50);
+        float clampedYRoll = Mathf.Clamp(yThrow * 30, -30, 30);
+
+        transform.localPosition = new Vector3(clampedXPos, clampedYPos, transform.localPosition.z);
+
+        transform.localEulerAngles = new Vector3(-clampedYRoll, transform.localRotation.y, - clampedZRoll);
+    }
+}
