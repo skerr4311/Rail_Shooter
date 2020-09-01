8463f89dcae1985e9e6eb8e83a0be3f34042fcc6
Steven Kerr <44153164+skerr4311@users.noreply.github.com>
2020-09-01 19:28:09 +1200

    added explosion to the enemy ships

--- Argon_Assalt_4/Assets/Scripts/Enemy.cs
+++ Argon_Assalt_4/Assets/Scripts/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,8 @@ public class Enemy : MonoBehaviour
 
     void OnParticleCollision(GameObject other)
     {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
         Destroy(gameObject);
     }
 }
