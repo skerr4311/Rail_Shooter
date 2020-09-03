1e1ff1caba1b50dd661e9d25c07802afdbd219ea
Steven Kerr <44153164+skerr4311@users.noreply.github.com>
2020-09-03 20:03:54 +1200

    updating the score to be static

--- Argon_Assalt_4/Assets/Scripts/ScoreBoard.cs
+++ Argon_Assalt_4/Assets/Scripts/ScoreBoard.cs
@@ -1,18 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreBoard : MonoBehaviour
 {
+    int score;
+    Text scoreText;
+    [SerializeField] int scorePerHit = 12;
     // Start is called before the first frame update
     void Start()
     {
-        
+        scoreText = GetComponent<Text>();
+        scoreText.text = score.ToString();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ScoreHit()
     {
-        
+        score = score + scorePerHit;
+        scoreText.text = score.ToString();
     }
 }
